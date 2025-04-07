3b2ec20da37a52804a0e00720790fd57e4944fdd
Appel Updater <appelupdater@gmail.com>
2025-04-07 17:40:22 +0300

    Add Custom Events to CheckboxSelect Component

--- dist/components/checkbox-select.js
+++ dist/components/checkbox-select.js
@@ -5,9 +5,12 @@ class CheckboxSelect extends HTMLElement {
     mainWrapper = document.createElement('div');
     valueElement = document.createElement('div');
     optionContainer = document.createElement('div');
+    isWidthFixed = false;
+    observer = null;
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.isWidthFixed = this.hasAttribute('width');
         // this.addEventListener('click', () => {\
         // console.log('Checkbox Select Created!');
         // console.log(this.querySelectorAll('option'));
@@ -16,6 +19,26 @@ class CheckboxSelect extends HTMLElement {
         this.selectedOptions = [];
         if (!this.shadowRoot)
             return;
+        this.initElement();
+        this.observer = new MutationObserver(() => (this.shadowRoot) ? this.initElement(this.isExpanded) : null);
+        this.observer.observe(this, {
+            childList: true,
+            subtree: true
+        });
+        // this.shadowRoot.innerHTML = `
+        // <div>CheckBox Select</div>
+        // `;
+        // });
+    }
+    initElement(expanded) {
+        if (!this.shadowRoot)
+            return;
+        // this.optionContainer.innerHTML = '';
+        this.mainWrapper.innerHTML = '';
+        if (expanded) {
+            this.setAttribute('expanded', String(expanded));
+            this.isExpanded = expanded;
+        }
         this.mainWrapper.style.display = 'inline-block';
         this.mainWrapper.style.border = '1px solid #727272';
         this.mainWrapper.style.borderRadius = '2px';
@@ -31,37 +54,46 @@ class CheckboxSelect extends HTMLElement {
         this.shadowRoot.appendChild(this.mainWrapper);
         this.mainWrapper.appendChild(this.valueElement);
         this.mainWrapper.style.cursor = 'pointer';
+        console.log('This.innerHTML!');
+        console.log(this.innerHTML);
         const width = this.initOptions(this.mainWrapper);
-        this.mainWrapper.style.width = (width) + 'px';
+        this.mainWrapper.style.width = (this.isWidthFixed && typeof this.getAttribute('width') === 'string') ? String(this.getAttribute('width')) + 'px' : ((width + 10) + 'px');
         this.mainWrapper.style.padding = '0 5px';
         this.valueElement.addEventListener('mousedown', () => {
             if (this.isExpanded == false) {
                 this.optionContainer.style.display = '';
+                this.optionContainer.style.zIndex = '4';
                 this.isExpanded = true;
+                this.dispatchEvent(new CustomEvent('expanded'));
             }
             else {
                 this.optionContainer.style.display = 'none';
+                this.optionContainer.style.zIndex = '';
                 this.isExpanded = false;
             }
         });
         window.addEventListener('mousedown', (e) => {
-            if (!(e.target instanceof CheckboxSelect)) {
+            // if (!(e.target instanceof CheckboxSelect)) {
+            if (e.target != this) {
                 this.optionContainer.style.display = 'none';
+                this.optionContainer.style.zIndex = '';
                 this.isExpanded = false;
             }
         });
         if (!this.hasAttribute('expanded')) {
             this.optionContainer.style.display = 'none';
+            this.optionContainer.style.zIndex = '';
             this.isExpanded = false;
         }
-        // this.shadowRoot.innerHTML = `
-        // <div>CheckBox Select</div>
-        // `;
-        // });
     }
-    initOptions(shadowRoot) {
+    initOptions(shadowRoot, refreshOptions) {
+        // if (refreshOptions) {
+        //     this.optionContainer.innerHTML = ''; // Очищаем innerHTML для повторного рендеринга опций
+        // }
+        console.log('Init Options!');
         let resultWidth = 5; // Отступы для красоты
         const options = this.querySelectorAll('option');
+        console.log(options);
         this.optionContainer.style.position = 'absolute';
         this.optionContainer.style.display = 'inline-block';
         this.optionContainer.style.backgroundColor = '#ffffff';
@@ -83,17 +115,50 @@ class CheckboxSelect extends HTMLElement {
             checkbox.style.display = 'inline-block';
             checkbox.style.cursor = 'pointer';
             checkbox.checked = option.selected;
+            checkbox.disabled = option.disabled;
             checkbox.id = `checkbox-select-option${index}`;
             (checkbox.checked) ? this.selectedOptions.push(option) : (this.selectedOptions.includes(option)) ? this.selectedOptions.splice(this.selectedOptions.indexOf(option), 1) : null;
-            container.htmlFor = checkbox.id;
+            // container.htmlFor = checkbox.id;
             option.style.display = 'inline-block';
-            option.id = String(index);
-            checkbox.addEventListener('change', () => {
+            // option.id = String(index);
+            checkbox.addEventListener('change', (e) => {
                 (checkbox.checked) ? this.selectedOptions.push(option) : this.selectedOptions.splice(this.selectedOptions.indexOf(option), 1);
                 this.updateSelectedOptions();
+                this.dispatchEvent(new CustomEvent('option-click', {
+                    detail: {
+                        optionElement: option,
+                        optionType: (option.hasAttribute('button')) ? 'button' : 'checkbox',
+                        value: option.value
+                    }
+                }));
             });
-            container.appendChild(checkbox);
+            if (option.hasAttribute('button')) {
+                // container.addEventListener('click', () => {
+                //     this.dispatchEvent(new CustomEvent('option-click', {
+                //         detail: {
+                //             optionElement: option,
+                //             optionType: (option.hasAttribute('button')) ? 'button' : 'checkbox',
+                //             value: option.value
+                //         }
+                //     }));
+                // });
+            }
+            else {
+                container.appendChild(checkbox);
+            }
             container.appendChild(option);
+            if (option.hasAttribute('button')) {
+                container.addEventListener('click', () => {
+                    this.dispatchEvent(new CustomEvent('option-click', {
+                        detail: {
+                            optionElement: option,
+                            optionType: (option.hasAttribute('button')) ? 'button' : 'checkbox',
+                            value: option.value
+                        }
+                    }));
+                });
+                // container.style.padding = '0 5px';
+            }
             this.optionContainer.appendChild(container);
             // option.addEventListener('click', () => {
             // result += option.offsetWidth;
@@ -110,7 +175,8 @@ class CheckboxSelect extends HTMLElement {
         });
         this.mainWrapper.style.position = 'relative';
         // console.log(this.optionContainer.offsetLeft - this.mainWrapper.offsetWidth);
-        this.optionContainer.style.width = (resultWidth + 10 /* padding (padding-left: 5px, padding-right: 5px) от mainWrapper */) + 'px';
+        // this.optionContainer.style.width = (resultWidth + 10 /* padding (padding-left: 5px, padding-right: 5px) от mainWrapper */) + 'px';
+        this.optionContainer.style.width = '100%';
         this.optionContainer.style.left = '-1px';
         this.optionContainer.style.marginTop = '5px';
         return resultWidth;
@@ -123,10 +189,20 @@ class CheckboxSelect extends HTMLElement {
         });
         // this.valueElement.innerText = selectedOptionText.join(', ') || 'Выберите...'; /* ▼ */
         this.valueElement.style.display = 'flex';
-        this.valueElement.innerHTML = `
-            <div style="width: 92%; /* background-color: red; */ text-align: left;">${selectedOptionText.join(', ') || 'Выберите...'}</div>
-            <div style="width: 8%; /* background-color: blue */; text-align: right;">▼</div>
+        const valueTextStyles = `
+            width: 100%;
+            ${(this.isWidthFixed) ? `
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            ` : ``}            
+            text-align: left;
         `;
+        this.valueElement.innerHTML = `
+            <div style="${valueTextStyles}">${selectedOptionText.join(', ') || 'Выберите...'}</div>
+            <div style="width: 20px; /* background-color: blue */; text-align: right;">▼</div>
+        `;
+        this.valueElement.title = (this.isWidthFixed) ? selectedOptionText.join(', ') : '';
     }
 }
 customElements.define('checkbox-select', CheckboxSelect);
--- src/components/checkbox-select.ts
+++ src/components/checkbox-select.ts
@@ -4,9 +4,12 @@ class CheckboxSelect extends HTMLElement {
     private mainWrapper: HTMLDivElement = document.createElement('div');
     private valueElement: HTMLDivElement = document.createElement('div');
     private optionContainer: HTMLDivElement = document.createElement('div');
+    private isWidthFixed: boolean = false;
+    private observer: MutationObserver | null = null;
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.isWidthFixed = this.hasAttribute('width');
         // this.addEventListener('click', () => {\
         // console.log('Checkbox Select Created!');
         // console.log(this.querySelectorAll('option'));
@@ -14,6 +17,27 @@ class CheckboxSelect extends HTMLElement {
         this.isExpanded = false;
         this.selectedOptions = [];
         if (!this.shadowRoot) return;
+        this.initElement();
+
+        this.observer = new MutationObserver(() => (this.shadowRoot) ? this.initElement(this.isExpanded) : null);
+        this.observer.observe(this, {
+            childList: true,
+            subtree: true
+        })
+        // this.shadowRoot.innerHTML = `
+        // <div>CheckBox Select</div>
+        // `;
+        // });
+    }
+
+    private initElement(expanded?: boolean): void {
+        if (!this.shadowRoot) return;
+        // this.optionContainer.innerHTML = '';
+        this.mainWrapper.innerHTML = '';
+        if (expanded) {
+            this.setAttribute('expanded', String(expanded));
+            this.isExpanded = expanded;
+        }
         this.mainWrapper.style.display = 'inline-block';
         this.mainWrapper.style.border = '1px solid #727272';
         this.mainWrapper.style.borderRadius = '2px';
@@ -29,38 +53,47 @@ class CheckboxSelect extends HTMLElement {
         this.shadowRoot.appendChild(this.mainWrapper);
         this.mainWrapper.appendChild(this.valueElement);
         this.mainWrapper.style.cursor = 'pointer';
+        console.log('This.innerHTML!');
+        console.log(this.innerHTML);
         const width: number = this.initOptions(this.mainWrapper);
-        this.mainWrapper.style.width = (width) + 'px';
+        this.mainWrapper.style.width = (this.isWidthFixed && typeof this.getAttribute('width') === 'string') ? String(this.getAttribute('width')) + 'px' : ((width + 10) + 'px');
         this.mainWrapper.style.padding = '0 5px';
 
         this.valueElement.addEventListener('mousedown', () => {
             if (this.isExpanded == false) {
                 this.optionContainer.style.display = '';
+                this.optionContainer.style.zIndex = '4';
                 this.isExpanded = true;
+                this.dispatchEvent(new CustomEvent('expanded'));
             } else {
                 this.optionContainer.style.display = 'none';
+                this.optionContainer.style.zIndex = '';
                 this.isExpanded = false;
             }
         });
         window.addEventListener('mousedown', (e: MouseEvent) => {
-            if (!(e.target instanceof CheckboxSelect)) {
+            // if (!(e.target instanceof CheckboxSelect)) {
+            if ((e.target as CheckboxSelect) != this) {
                 this.optionContainer.style.display = 'none';
+                this.optionContainer.style.zIndex = '';
                 this.isExpanded = false;
             }
         });
         if (!this.hasAttribute('expanded')) {
             this.optionContainer.style.display = 'none';
+            this.optionContainer.style.zIndex = '';
             this.isExpanded = false;
         }
-        // this.shadowRoot.innerHTML = `
-        // <div>CheckBox Select</div>
-        // `;
-        // });
     }
 
-    private initOptions(shadowRoot: ShadowRoot | HTMLElement): number {
+    private initOptions(shadowRoot: ShadowRoot | HTMLElement, refreshOptions?: boolean): number {
+        // if (refreshOptions) {
+        //     this.optionContainer.innerHTML = ''; // Очищаем innerHTML для повторного рендеринга опций
+        // }
+        console.log('Init Options!');
         let resultWidth: number = 5; // Отступы для красоты
         const options: NodeListOf<HTMLOptionElement> = this.querySelectorAll('option');
+        console.log(options);
         this.optionContainer.style.position = 'absolute';
         this.optionContainer.style.display = 'inline-block';
         this.optionContainer.style.backgroundColor = '#ffffff';
@@ -82,17 +115,49 @@ class CheckboxSelect extends HTMLElement {
             checkbox.style.display = 'inline-block';
             checkbox.style.cursor = 'pointer';
             checkbox.checked = option.selected;
+            checkbox.disabled = option.disabled;
             checkbox.id = `checkbox-select-option${index}`;
             (checkbox.checked) ? this.selectedOptions.push(option) : (this.selectedOptions.includes(option)) ? this.selectedOptions.splice(this.selectedOptions.indexOf(option), 1) : null;
-            container.htmlFor = checkbox.id;            
+            // container.htmlFor = checkbox.id;
             option.style.display = 'inline-block';
-            option.id = String(index);
-            checkbox.addEventListener('change', () => {
+            // option.id = String(index);
+            checkbox.addEventListener('change', (e: Event) => {
                 (checkbox.checked) ? this.selectedOptions.push(option) : this.selectedOptions.splice(this.selectedOptions.indexOf(option), 1);
                 this.updateSelectedOptions();
+                this.dispatchEvent(new CustomEvent('option-click', {
+                    detail: {
+                        optionElement: option,
+                        optionType: (option.hasAttribute('button')) ? 'button' : 'checkbox',
+                        value: option.value
+                    }
+                }));
             });
-            container.appendChild(checkbox);
+            if (option.hasAttribute('button')) {
+                // container.addEventListener('click', () => {
+                //     this.dispatchEvent(new CustomEvent('option-click', {
+                //         detail: {
+                //             optionElement: option,
+                //             optionType: (option.hasAttribute('button')) ? 'button' : 'checkbox',
+                //             value: option.value
+                //         }
+                //     }));
+                // });
+            } else {
+                container.appendChild(checkbox);
+            }
             container.appendChild(option);
+            if (option.hasAttribute('button')) {
+                container.addEventListener('click', () => {
+                    this.dispatchEvent(new CustomEvent('option-click', {
+                        detail: {
+                            optionElement: option,
+                            optionType: (option.hasAttribute('button')) ? 'button' : 'checkbox',
+                            value: option.value
+                        }
+                    }));
+                });
+                // container.style.padding = '0 5px';
+            }
             this.optionContainer.appendChild(container);
             // option.addEventListener('click', () => {
             // result += option.offsetWidth;
@@ -109,7 +174,8 @@ class CheckboxSelect extends HTMLElement {
         });
         this.mainWrapper.style.position = 'relative';
         // console.log(this.optionContainer.offsetLeft - this.mainWrapper.offsetWidth);
-        this.optionContainer.style.width = (resultWidth + 10 /* padding (padding-left: 5px, padding-right: 5px) от mainWrapper */) + 'px';
+        // this.optionContainer.style.width = (resultWidth + 10 /* padding (padding-left: 5px, padding-right: 5px) от mainWrapper */) + 'px';
+        this.optionContainer.style.width = '100%';
         this.optionContainer.style.left = '-1px';
         this.optionContainer.style.marginTop = '5px';
         return resultWidth;
@@ -123,10 +189,20 @@ class CheckboxSelect extends HTMLElement {
         });
         // this.valueElement.innerText = selectedOptionText.join(', ') || 'Выберите...'; /* ▼ */
         this.valueElement.style.display = 'flex';
-        this.valueElement.innerHTML = `
-            <div style="width: 92%; /* background-color: red; */ text-align: left;">${selectedOptionText.join(', ') || 'Выберите...'}</div>
-            <div style="width: 8%; /* background-color: blue */; text-align: right;">▼</div>
+        const valueTextStyles: string = `
+            width: 100%;
+            ${(this.isWidthFixed) ? `
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            ` : ``}            
+            text-align: left;
         `;
+        this.valueElement.innerHTML = `
+            <div style="${valueTextStyles}">${selectedOptionText.join(', ') || 'Выберите...'}</div>
+            <div style="width: 20px; /* background-color: blue */; text-align: right;">▼</div>
+        `;
+        this.valueElement.title = (this.isWidthFixed) ? selectedOptionText.join(', ') : '';
     }
 }
 
