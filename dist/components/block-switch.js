35bc7fb3d427b2d787895bd637cc893dde67ddda
Appel Updater <appelupdater@gmail.com>
2025-04-12 15:36:33 +0300

    Add Number Data Type Block Switch to Modal Window

--- dist/components/block-switch.js
+++ dist/components/block-switch.js
@@ -1,8 +1,10 @@
 "use strict";
+const OPTION_STYLE_DISPLAY = 'inline-block';
 class BlockSwitch extends HTMLElement {
     // private forElement: string;
     slotElement = document.createElement('slot');
     observer; // Включить, когда он понадобится
+    styleElement = document.createElement('style');
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
@@ -11,16 +13,34 @@ class BlockSwitch extends HTMLElement {
     updateElement(initial) {
         if (!this.shadowRoot)
             return;
+        this.slotElement = document.createElement('slot');
+        this.slotElement.style.display = 'none';
         const forElement = (typeof this.getAttribute('for') === 'string') ? document.querySelector(String(this.getAttribute('for'))) : null;
-        this.shadowRoot.innerHTML = this.innerHTML;
-        // this.shadowRoot.innerHTML = `<slot></slot>`; // Не работает
+        this.styleElement = document.createElement('style');
+        this.styleElement.textContent = `
+                select, input, button {
+                    padding: 5px;
+                    cursor: pointer;
+                }
+        `;
+        this.shadowRoot.append(this.styleElement);
+        // this.shadowRoot.innerHTML = this.innerHTML;
+        // this.shadowRoot.innerHTML = `
+        //     <div>
+        //         <slot></slot>
+        //     </div>
+        // `;
+        this.shadowRoot.innerHTML = `<slot></slot>`;
         if (forElement) {
             const selectElements = [CheckboxSelect, HTMLSelectElement, CustomSelect];
             selectElements.forEach((Element) => {
                 if (forElement instanceof Element) {
-                    const thisChildNodes = Array.from(this.shadowRoot.querySelectorAll('block-switch-option'));
+                    // const thisChildNodes: HTMLElement[] = Array.from(this.shadowRoot!.querySelectorAll('block-switch-option'));
+                    const thisChildNodes = Array.from(this.querySelectorAll('block-switch-option'));
+                    console.info('THIS CHILD NODES!');
+                    console.log(thisChildNodes);
                     const forChildNodes = Array.from(forElement.childNodes);
-                    // console.log(thisChildNodes);
+                    console.log(forChildNodes);
                     forChildNodes.forEach((node) => {
                         // if (node instanceof HTMLElement) {
                         //     // console.log(node.tagName);
@@ -30,19 +50,19 @@ class BlockSwitch extends HTMLElement {
                             // if (thisChildNodes.includes(node.value)) {
                             //     // console.log(this.shadowRoot!.querySelector(node.value));
                             // }
-                            const element = this.shadowRoot.querySelector(`block-switch-option[value="${node.value}"]:not(other-value)`);
-                            const otherValue = this.shadowRoot.querySelector(`block-switch-option[other-value]`);
+                            const element = this.querySelector(`block-switch-option[value="${node.value}"]:not(other-value)`);
+                            const otherValue = this.querySelector(`block-switch-option[other-value]`);
                             if (element instanceof HTMLElement) {
                                 // if (thisChildNodes.includes(node.value)) {
                                 if (thisChildNodes.includes(element)) {
-                                    element.style.display = (node.selected) ? 'block' : 'none';
+                                    element.style.display = (node.selected) ? OPTION_STYLE_DISPLAY : 'none';
                                     if (otherValue instanceof HTMLElement && node.selected)
                                         otherValue.style.display = 'none';
                                 }
                                 else {
                                     element.style.display = 'none';
                                     if (otherValue instanceof HTMLElement)
-                                        otherValue.style.display = 'block';
+                                        otherValue.style.display = OPTION_STYLE_DISPLAY;
                                 }
                             }
                         }
@@ -51,8 +71,6 @@ class BlockSwitch extends HTMLElement {
             });
         }
         // (this.shadowRoot.querySelector('number-field') as HTMLElement).style.display = 'none';
-        this.slotElement = document.createElement('slot');
-        this.slotElement.style.display = 'none';
     }
     connectedCallback() {
         this.updateElement(true);
@@ -62,6 +80,7 @@ class BlockSwitch extends HTMLElement {
         // console.log(forElement);
         forElement.addEventListener('value-change', (event) => {
             this.updateElement();
+            // console.log(forElement);
             // if (event instanceof CustomEvent) {
             //     alert(`VALUE CHANGE: ` + event.detail.value);
             // }
--- src/components/block-switch.ts
+++ src/components/block-switch.ts
@@ -1,7 +1,10 @@
+const OPTION_STYLE_DISPLAY: string = 'inline-block';
+
 class BlockSwitch extends HTMLElement {
     // private forElement: string;
     private slotElement: HTMLSlotElement = document.createElement('slot');
     private observer: MutationObserver; // Включить, когда он понадобится
+    private styleElement: HTMLStyleElement = document.createElement('style');
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
@@ -10,16 +13,35 @@ class BlockSwitch extends HTMLElement {
 
     private updateElement(initial?: boolean) {
         if (!this.shadowRoot) return;
+        this.slotElement = document.createElement('slot');
+        this.slotElement.style.display = 'none';
         const forElement: HTMLElement | null = (typeof this.getAttribute('for') === 'string') ? document.querySelector(String(this.getAttribute('for'))) : null;
-        this.shadowRoot.innerHTML = this.innerHTML;
-        // this.shadowRoot.innerHTML = `<slot></slot>`; // Не работает
+        this.styleElement = document.createElement('style');
+        this.styleElement.textContent = `
+                select, input, button {
+                    padding: 5px;
+                    cursor: pointer;
+                }
+        `;
+        this.shadowRoot.append(this.styleElement);
+        // this.shadowRoot.innerHTML = this.innerHTML;
+        // this.shadowRoot.innerHTML = `
+        //     <div>
+        //         <slot></slot>
+        //     </div>
+        // `;
+        this.shadowRoot.innerHTML = `<slot></slot>`;
+        
         if (forElement) {
             const selectElements = [CheckboxSelect, HTMLSelectElement, CustomSelect];
             selectElements.forEach((Element) => {
                 if (forElement instanceof Element) {
-                    const thisChildNodes: HTMLElement[] = Array.from(this.shadowRoot!.querySelectorAll('block-switch-option'));
+                    // const thisChildNodes: HTMLElement[] = Array.from(this.shadowRoot!.querySelectorAll('block-switch-option'));
+                    const thisChildNodes: HTMLElement[] = Array.from(this.querySelectorAll('block-switch-option'));
+                    console.info('THIS CHILD NODES!');
+                    console.log(thisChildNodes);
                     const forChildNodes: ChildNode[] = Array.from(forElement.childNodes);
-                    // console.log(thisChildNodes);
+                    console.log(forChildNodes);
                     forChildNodes.forEach((node: ChildNode) => {
                         // if (node instanceof HTMLElement) {
                         //     // console.log(node.tagName);
@@ -29,16 +51,16 @@ class BlockSwitch extends HTMLElement {
                             // if (thisChildNodes.includes(node.value)) {
                             //     // console.log(this.shadowRoot!.querySelector(node.value));
                             // }
-                            const element: HTMLElement | null = this.shadowRoot!.querySelector(`block-switch-option[value="${node.value}"]:not(other-value)`);
-                            const otherValue: HTMLElement | null = this.shadowRoot!.querySelector(`block-switch-option[other-value]`);
+                            const element: HTMLElement | null = this.querySelector(`block-switch-option[value="${node.value}"]:not(other-value)`);
+                            const otherValue: HTMLElement | null = this.querySelector(`block-switch-option[other-value]`);
                             if (element instanceof HTMLElement) {
                                 // if (thisChildNodes.includes(node.value)) {
                                 if (thisChildNodes.includes(element)) {
-                                    element.style.display = (node.selected) ? 'block' : 'none';
+                                    element.style.display = (node.selected) ? OPTION_STYLE_DISPLAY : 'none';
                                     if (otherValue instanceof HTMLElement && node.selected) otherValue.style.display = 'none';
                                 } else {
                                     element.style.display = 'none';
-                                    if (otherValue instanceof HTMLElement) otherValue.style.display = 'block';
+                                    if (otherValue instanceof HTMLElement) otherValue.style.display = OPTION_STYLE_DISPLAY;
                                 }
                             }
                         }
@@ -47,8 +69,6 @@ class BlockSwitch extends HTMLElement {
             });
         }
         // (this.shadowRoot.querySelector('number-field') as HTMLElement).style.display = 'none';
-        this.slotElement = document.createElement('slot');
-        this.slotElement.style.display = 'none';
     }
 
     connectedCallback(): void {
@@ -58,6 +78,7 @@ class BlockSwitch extends HTMLElement {
         // console.log(forElement);
         forElement.addEventListener('value-change', (event: Event) => {
             this.updateElement();
+            // console.log(forElement);
             // if (event instanceof CustomEvent) {
             //     alert(`VALUE CHANGE: ` + event.detail.value);
             // }
